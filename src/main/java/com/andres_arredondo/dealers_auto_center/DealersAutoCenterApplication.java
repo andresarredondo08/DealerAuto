7e2a09a50eae66ce55e58a094720ec6885ad06f5
Andres Felipe Arredondo <andres.arredondo9608@gmail.com>
2026-04-27 09:59:46 -0500

    Initial Commit - Spring boot base project

---
+++ src/main/java/com/andres_arredondo/dealers_auto_center/DealersAutoCenterApplication.java
@@ -0,0 +1,13 @@
+package com.andres_arredondo.dealers_auto_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealersAutoCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealersAutoCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andres_arredondo/dealers_auto_center/DealersAutoCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres_arredondo.dealers_auto_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealersAutoCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
